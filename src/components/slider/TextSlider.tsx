ab1afe2fcfaec06d06eb83b50de7eb8ad960405d
imranprof <imranpers@gmail.com>
2025-07-05 21:10:31 +0600

    style: modify slider style

--- src/components/slider/TextSlider.tsx
+++ src/components/slider/TextSlider.tsx
@@ -7,13 +7,13 @@ const TextSlider = () => {
       <Marquee>
         <h2 className="uppercase text-[61px] py-[19px] font-bold opacity-80 text-white [-webkit-text-stroke:_2px_white] [text-stroke:_2px_white] [-webkit-text-fill-color:transparent]">
           Creative
-          <span> <Image src="/assets/imgs/icon/spark-1.png" alt="icon" width={47} height={47} className="inline-flex items-center" /></span>
+          <span className="inline-flex items-center mx-[25px]"> <Image src="/assets/imgs/icon/spark-1.png" alt="icon" width={47} height={47} /></span>
           Solutions
-          <span><Image src="/assets/imgs/icon/spark-2.png" alt="icon" width={47} height={47} className="inline-flex items-center" /></span>
+          <span className="inline-flex items-center mx-[25px]"><Image src="/assets/imgs/icon/spark-2.png" alt="icon" width={47} height={47} /></span>
           Expert Guidance
-          <span><Image src="/assets/imgs/icon/spark-1.png" alt="icon" width={47} height={47} className="inline-flex items-center" /></span>
+          <span className="inline-flex items-center mx-[25px]"><Image src="/assets/imgs/icon/spark-1.png" alt="icon" width={47} height={47} /></span>
           Consulting
-          <span><Image src="/assets/imgs/icon/spark-2.png" alt="icon" width={47} height={47} className="inline-flex items-center" /></span>
+          <span className="inline-flex items-center mx-[25px]"><Image src="/assets/imgs/icon/spark-2.png" alt="icon" width={47} height={47} /></span>
         </h2>
       </Marquee>
     </div>
